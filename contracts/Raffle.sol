6b402d904f7a02405337d4d901e8ebb5234cca04
Deept Shukla <deept@ip-192-168-1-13.ap-southeast-1.compute.internal>
2022-08-03 20:21:06 +0530

    add basic raffle contract

---
+++ contracts/Raffle.sol
@@ -0,0 +1,35 @@
+// Raffle
+// Enter the lottery (paying some ammount)
+// Pick a random winner (verifiable random)
+// Winner be selected every X minute -> Automated
+// Chainlink oracle -> randomness, automated execution (Chainlink keeper)
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+error Raffle__NotEnoughEthEntered();
+
+contract Raffle {
+    /*State Variables */
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players; // We want the address to be payable, as we will pay one player the winning ammount
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthEntered();
+        }
+        s_players.push(payable(msg.sender));
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
